f4c0863c79722df192765d14ebf3f6a1741415cc
Rocket IT <rocketit@Mac-11tt.local>
2021-05-26 10:24:41 -0400

    created small random number generator to test passing data between main.py and responseToEmail.py

--- responseToEmail.py
+++ responseToEmail.py
@@ -1,8 +1,19 @@
+import random
+
 def test_para(arg):
     print(arg + "Response to Email")
     arg=="end"
     return arg
 
+# True or False generator to test and make sure word will pass back and forth between main.py and responseToEmail.py
+
 def trueOrFalse(word):
-    word=True
+
+    random_number = random.randint(0,10)
+    if (random_number % 2):
+        word=True
+    elif (random_number == 0):
+        word = 'Zero'
+    else:
+        word=False
     return word
