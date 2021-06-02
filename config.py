1ad3a4ad0209bf9f4abe1e267068d5d07d527eee
KLawsonDevelopment <klawsondevelopment@gmail.com>
2021-06-02 09:24:48 -0400

    copied parts of microsoft python test module under MIT license to get login to work.

---
+++ config.py
@@ -0,0 +1,16 @@
+# Used from Microsoft, able to be copied and used at will under MIT License, per Microsoft
+
+"""Configuration settings for console app using device flow authentication
+"""
+
+CLIENT_ID = 'dcde5c47-90ef-4fbc-acae-07a616c1c6d6'
+
+AUTHORITY_URL = 'https://login.microsoftonline.com/common'
+RESOURCE = 'https://graph.microsoft.com'
+API_VERSION = 'beta'
+
+# This code can be removed after configuring CLIENT_ID and CLIENT_SECRET above.
+if 'ENTER_YOUR' in CLIENT_ID:
+    print('ERROR: config.py does not contain valid CLIENT_ID.')
+    import sys
+    sys.exit(1)
--- main.py
+++ main.py
@@ -6,6 +6,8 @@
 
 #Importing all files to reference later
 
+import pprint
+import config
 import requests
 import headerOfEmail
 import to_sender
@@ -13,14 +15,60 @@ import from_sender
 import bodyOfEmail
 import responseToEmail
 
+from helpers import api_endpoint, device_flow_session, profile_photo, \
+    send_mail, sharing_link, upload_file
 
 
 
-arg = "I am "
-word = 'no'
 
-response = requests.get("https://api.publicapis.org/entries")
-print (response.json())
+def sendmail_sample(session):
+    """Send email from authenticated user.
+
+    session = requests.Session() instance with a valid access token for
+              Microsoft Graph in its default HTTP headers
+
+    This sample retrieves the user's profile photo, uploads it to OneDrive,
+    creates a view-only sharing link for the photo, and sends an email
+    with the photo attached.
+
+    The code in this function includes many print statements to provide
+    information about which endpoints are being called and the status and
+    size of Microsoft Graph responses. This information is helpful for
+    understanding how the sample works with Graph, but would not be included
+    in a typical production application.
+    """
+
+    print('\nGet user profile ---------> https://graph.microsoft.com/beta/me')
+    user_profile = session.get(api_endpoint('me'))
+    print(28*' ' + f'<Response [{user_profile.status_code}]>', f'bytes returned: {len(user_profile.text)}\n')
+    if not user_profile.ok:
+        pprint.pprint(user_profile.json()) # display error
+        return
+    user_data = user_profile.json()
+    email = user_data['mail']
+    display_name = user_data['displayName']
+
+    print(f'Your name ----------------> {display_name}')
+    print(f'Your email ---------------> {email}')
+    email_to = input(f'Send-to (ENTER=self) -----> ') or email
+
+    print('\nGet profile photo --------> https://graph.microsoft.com/beta/me/photo/$value')
+    photo, photo_status_code, _, profile_pic = profile_photo(session, save_as='me')
+    print(28*' ' + f'<Response [{photo_status_code}]>',
+          f'bytes returned: {len(photo)}, saved as: {profile_pic}')
+    if not 200 <= photo_status_code <= 299:
+        return
+
+if __name__ == '__main__':
+    GRAPH_SESSION = device_flow_session(config.CLIENT_ID)
+    if GRAPH_SESSION:
+        sendmail_sample(GRAPH_SESSION)
+
+# arg = "I am "
+# word = 'no'
+
+# response = requests.get("https://api.publicapis.org/entries")
+# print (response.json())
 
 #Generic call function with argument to test data passing
 
