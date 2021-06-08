da6a920ce4ca2dfc08ee0b613bb284fd3aa3863d
KLawsonDevelopment <klawsondevelopment@gmail.com>
2021-06-08 12:30:41 -0400

    Rerunning looping script to test and see if all logic works

--- dataParsing.py
+++ dataParsing.py
@@ -19,34 +19,34 @@ def dataGrab(iD, session):
 
     mail_data = json.dumps(mail_data_get.json(), indent=4, sort_keys=True)
 
-    # print ('Calling for Header via Script\n')
+    print ('Calling for Header via Script\n')
 
-    # time.sleep(3)
+    time.sleep(3)
 
-    # headerGrade = headerOfEmail.headerGet(iD, session)
+    headerGrade = headerOfEmail.headerGet(iD, session)
 
-    # print ('Calling for To via Script\n')
+    print ('Calling for To via Script\n')
 
-    # time.sleep(3)
+    time.sleep(3)
 
-    # toGrade = to_sender.toGet(iD, session)
+    toGrade = to_sender.toGet(iD, session)
 
-    # print ('Calling for From via Script\n')
+    print ('Calling for From via Script\n')
 
-    # time.sleep(3)
+    time.sleep(3)
 
     fromGrade = from_sender.fromGet(iD, session)
 
-    # print ('Calling for Body via Script\n')
+    print ('Calling for Body via Script\n')
 
-    # time.sleep(3)
+    time.sleep(3)
 
-    # bodyGrade = bodyOfEmail.bodyGet(iD, session)
+    bodyGrade = bodyOfEmail.bodyGet(iD, session)
 
-    # print ('Calling for Response via Script')
+    print ('Calling for Response via Script')
 
-    # time.sleep(3)
+    time.sleep(3)
 
-    # responseToEmail.respondToEmail(iD, session, headerGrade, toGrade, fromGrade, bodyGrade)
+    responseToEmail.respondToEmail(iD, session, headerGrade, toGrade, fromGrade, bodyGrade)
 
-    # return
+    return
