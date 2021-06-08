000fbe126ca76fef4b909f2f697466869c9a0c71
KLawsonDevelopment <klawsondevelopment@gmail.com>
2021-06-08 11:09:40 -0400

    fixed style sheets, added new parameters to make the email easier to understand for the user, and cleaned up some text.

--- responseToEmail.py
+++ responseToEmail.py
@@ -10,6 +10,52 @@ from helpers import api_endpoint, device_flow_session, profile_photo, \
 
 def respondToEmail(iD, session, headerGrade, toGrade, fromGrade, bodyGrade):
 
+    headerGoB = str
+    toGoB = str
+    fromGoB = str
+    bodyGoB = str
+
+    if headerGrade > 0:
+        headerGoB = 'Good'
+    elif headerGrade == 0:
+        headerGoB = 'Neutral'
+    else:
+        headerGoB = 'Bad'
+
+    if toGrade > 0:
+        toGoB = 'Good'
+    elif toGrade == 0:
+        toGoB = 'Neutral'
+    else:
+        toGoB = 'Bad'
+
+    if fromGrade > 0:
+        fromGoB = 'Good'
+    elif fromGrade == 0:
+        fromGoB = 'Neutral'
+    else:
+        fromGoB = 'Bad'
+
+    if bodyGrade > 0:
+        bodyGoB = 'Good'
+    elif bodyGrade == 0:
+        bodyGoB = 'Neutral'
+    else:
+        bodyGoB = 'Bad'
+
+    totalGrade = headerGrade + toGrade + fromGrade + bodyGrade
+
+    goodOrBad = str
+
+    if totalGrade > 0:
+        goodOrBad = 'This email should be safe to open.'
+    elif totalGrade == 0:
+        goodOrBad = 'We are not sure if this email is safe to open.'
+    else:
+        goodOrBad = 'We believe this email should not be opened.'
+
+
+
     mail_data_get = session.get(api_endpoint((f'me/messages/{iD}')))
 
     to_data = json.dumps(mail_data_get.json(), indent=4, sort_keys=True)
@@ -19,7 +65,8 @@ def respondToEmail(iD, session, headerGrade, toGrade, fromGrade, bodyGrade):
     email_to = to_data_json['sender']['emailAddress']['address']
 
     with open('email.html') as template_file:
-        template = template_file.read().format(headerGrade=headerGrade, toGrade=toGrade, fromGrade=fromGrade, bodyGrade=bodyGrade)
+        template = template_file.read().format(headerGrade=headerGrade, toGrade=toGrade, fromGrade=fromGrade, bodyGrade=bodyGrade,
+                                                headerGoB=headerGoB, toGoB=toGoB, fromGoB=fromGoB, bodyGoB=bodyGoB, goodOrBad=goodOrBad)
 
     send_response = send_mail(session=session,
                               subject='Results of your Phishing Email',
